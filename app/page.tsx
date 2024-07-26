82dc4bc8667c203f748db76a8466ab110bde4798
anasmunawar2 <anasmunawar2@gmail.com>
2024-07-27 00:33:41 +0500

    hompepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
+import PateintForm from "@/components/forms/PateintForm";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl underline">Home</h1>
+    <div className="flex h-screen max-h-screen">
+      {/* Todo: OTP Verification */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt=""
+            className="mb-12 h-10 w-fit"
+          />
+          <PateintForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 PrimePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
