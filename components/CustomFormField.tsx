2c1c826ed0f26d20c15e3b5f7b4685b2de497ed6
anasmunawar2 <anasmunawar2@gmail.com>
2024-07-27 00:58:02 +0500

    shadcn-form

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Control } from "react-hook-form";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+
+interface CustomProps {
+  control: Control<any>;
+}
+
+const CustomFormField = ({ control }: CustomProps) => {
+  return (
+    <div>
+      <FormField
+        control={control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input placeholder="shadcn" {...field} />
+            </FormControl>
+            <FormDescription>This is your public display name.</FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+};
+
+export default CustomFormField;
