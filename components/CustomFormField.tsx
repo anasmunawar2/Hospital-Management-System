1ce958d218e0fd1b2b3ac2a1c34b419b6f9ac492
anasmunawar2 <anasmunawar2@gmail.com>
2024-07-30 01:55:15 +0500

    custom-fields

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -11,6 +11,7 @@ import {
 import { Input } from "./ui/input";
 import { FormFieldType } from "./forms/PateintForm";
 import React from "react";
+import Image from "next/image";
 
 interface CustomProps {
   control: Control<any>;
@@ -27,7 +28,37 @@ interface CustomProps {
   renderSkeleton?: (field: any) => React.ReactNode;
 }
 
-const CustomFormField = ({ control, fieldType, name, label }: CustomProps) => {
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const { fieldType, iconSrc, iconAlt, placeholder } = props;
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              height={24}
+              width={24}
+              alt={iconAlt || "icon"}
+              className="ml-2"
+            />
+          )}
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      );
+    default:
+      break;
+  }
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label } = props;
   return (
     <div>
       <FormField
@@ -38,6 +69,9 @@ const CustomFormField = ({ control, fieldType, name, label }: CustomProps) => {
             {fieldType !== FormFieldType.CHECKBOX && label && (
               <FormLabel>{label}</FormLabel>
             )}
+            <RenderField field={field} props={props} />
+
+            <FormMessage className="shad-error" />
           </FormItem>
         )}
       />
